a9c873e577ac00ee3743ed4159fb4979d754489d
Kashifrazabb <Kashifrazabb@gmail.com>
2023-06-05 03:31:22 +0500

    a

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import "./db.js"
 import path from "path"
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
