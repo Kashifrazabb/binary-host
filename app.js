c8672957ef8c83844dd239f9fbe649ee70c09690
Kashifrazabb <Kashifrazabb@gmail.com>
2023-06-05 00:48:44 +0500

    cors

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ import path from "path"
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(path.resolve(), "assets")))
 app.set("view engine", "ejs")
